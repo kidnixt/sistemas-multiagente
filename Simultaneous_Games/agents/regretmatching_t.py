9403d363f86a99800f7e6b9185e4a1a2c213fcce
iturbideM <martiniturbidenoria@gmail.com>
2025-04-02 22:51:28 -0300

    regret matching done

--- Simultaneous_Games/agents/regretmatching_t.py
+++ Simultaneous_Games/agents/regretmatching_t.py
@@ -1,6 +1,8 @@
 import numpy as np
 from base.agent import Agent
 from base.game import SimultaneousGame, AgentID, ActionDict
+from itertools import product
+
 
 class RegretMatching(Agent):
 
@@ -17,35 +19,49 @@ class RegretMatching(Agent):
         np.random.seed(seed=seed)
 
     def regrets(self, played_actions: ActionDict) -> dict[AgentID, float]:
-        actions = played_actions.copy()
+        actions = played_actions.copy() # Accion conjunta
         a = actions[self.agent]
         g = self.game.clone()
         u = np.zeros(g.num_actions(self.agent), dtype=float)
-        # 
-        # TODO: calcular regrets
-        #
-        r = None
-        return r
+        
+        g.reset()
+        g.step(actions)
+        r_base = g.rewards[self.agent]
+
+        # Calcular los rewards de agente para cada acción conjunta
+        for act in range(g.num_actions(self.agent)):
+            g.reset()
+            actions[self.agent] = act
+            g.step(actions)
+            r = g.rewards[self.agent]
+            u[act] = r - r_base
+        
+        return u
     
     def regret_matching(self):
-        #
-        # TODO: calcular curr_policy y actualizar sum_policy
-        #
-        pass
+        random = np.full(self.game.num_actions(self.agent), 1/self.game.num_actions(self.agent))
+        if np.sum(self.cum_regrets) <= 0:
+            self.curr_policy = random
+        else:
+            self.curr_policy = np.maximum(self.cum_regrets,0)/np.sum(np.maximum(self.cum_regrets,0))
+
+        self.sum_policy += self.curr_policy
 
     def update(self) -> None:
         actions = self.game.observe(self.agent)
         if actions is None:
            return
         regrets = self.regrets(actions)
+
         self.cum_regrets += regrets
         self.regret_matching()
         self.niter += 1
+        
         self.learned_policy = self.sum_policy / self.niter
 
     def action(self):
         self.update()
-        return np.argmax(np.random.multinomial(1, self.curr_policy, size=1))
-    
+        return np.argmax(np.random.multinomial(1, self.curr_policy, size=1))    
+
     def policy(self):
         return self.learned_policy
